32a52432f4c96e811aea67514dd0536f2e554387
master <master@mukhea2.(none)>
2009-01-09 16:12:14 -0500

    added url

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(  name='midentify',
         author="Abhishek Mukherjee",
         author_email="linkinpark342@gmail.com",
         py_modules=['midentify'],
+        url='http://github.com/linkinpark342/midentify-py/',
         classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
