91dd28fb509eca1660f3e419e66b43d351b2b07d
master <master@mukhea2.(none)>
2009-01-09 16:07:56 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(  name='midentify',
+        version='0.1',
+        description="Identification of a file using MPlayer's midentify.sh script",
+        author="Abhishek Mukherjee",
+        author_email="linkinpark342@gmail.com",
+        py_modules=['midentify'],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: Public Domain',
+            'Topic :: Multimedia :: Video',
+            ]
+        )
